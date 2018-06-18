5bbb61019ed776f0e6dcdf5fe5b1ba91d93e992e
Ranga Surendra <rangasurendra@gmail.com>
2018-06-18 11:09:34 +0530

    Add files via upload

---
+++ ColorPicker/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ColorPicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblColor = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblColor
+            // 
+            this.lblColor.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblColor.Location = new System.Drawing.Point(0, 0);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(193, 94);
+            this.lblColor.TabIndex = 0;
+            this.lblColor.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblColor_MouseDown);
+            this.lblColor.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblColor_MouseUp);
+            // 
+            // txtColor
+            // 
+            this.txtColor.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtColor.Location = new System.Drawing.Point(0, 97);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(193, 20);
+            this.txtColor.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 117);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.lblColor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Color Picker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.TextBox txtColor;
+    }
+}
+
---
+++ ColorPicker/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace ColorPicker
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        static extern Int32 ReleaseDC(IntPtr hwnd, IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblColor_MouseDown(object sender, MouseEventArgs e)
+        {
+            var x = e.X;
+            var y = e.Y;
+        }
+
+        /// <summary>
+        /// Get the pixel color once mouse button released
+        /// </summary>
+        /// <param name="x">x position of the cursor</param>
+        /// <param name="y">y position of the cursor</param>
+        /// <returns>Color</returns>
+        static public System.Drawing.Color GetPixelColor(int x, int y)
+        {
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            uint pixel = GetPixel(hdc, x, y);
+            ReleaseDC(IntPtr.Zero, hdc);
+            Color color = Color.FromArgb((int)(pixel & 0x000000FF),
+                         (int)(pixel & 0x0000FF00) >> 8,
+                         (int)(pixel & 0x00FF0000) >> 16);
+            return color;
+        }
+
+        /// <summary>
+        /// Load the selected pixel's color in to background label, 
+        /// and code in to text box at the bottom
+        /// </summary>
+        private void lblColor_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+
+            lblColor.BackColor = GetPixelColor(Cursor.Position.X, Cursor.Position.Y);
+            txtColor.Text = HexConverter(lblColor.BackColor);
+            Clipboard.SetText(txtColor.Text);
+        }
+
+        /// <summary>
+        /// Converts color in to Hex Format
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        private static String HexConverter(System.Drawing.Color c)
+        {
+            return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+        }
+    }
+}
